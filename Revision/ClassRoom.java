dd1563819ad1790e6070fd78591ec3ca7d9d4e18
Jayshankar <kjayshankar182@gmail.com>
2026-07-06 13:39:03 +0530

    Hashing

---
+++ Revision/ClassRoom.java
@@ -0,0 +1,33 @@
+package Revision;
+import java.util.*;
+
+public class ClassRoom {
+
+    public static void majorityElement(int nums[]){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+            if(map.containsKey(nums[i])){
+            map.put(nums[i], map.get(nums[i]) + 1);
+            }
+            else  {
+                map.put(nums[i],1);
+            }
+        }
+        System.out.println(map);
+        
+        for(int key : map.keySet()){
+            if(map.get(key) >n/3){
+                System.out.println(key);
+            }
+        }
+
+    }
+
+
+    public static void main(String args[]){
+  int nums[] ={1, 2, 3, 4, 5, 6, 1, 4, 3, 4, 3, 5, 6, };
+     majorityElement(nums);
+    }
+    
+}
